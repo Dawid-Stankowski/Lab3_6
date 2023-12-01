adca5e991515d2116fa4dbc9f904dbe413a8a91e
Dawid-Stankowski <dawid441@interia.pl>
2023-12-01 11:51:18 +0100

    1.0

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,17 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
+
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        String str1 = new String("Test");
+        String str2 = new String("Test");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        // Porównaj za pomocą operatora ==
+        System.out.println("Porównanie za pomocą operatora ==:");
+        System.out.println(str1 == str2);
+        System.out.println();
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        // Porównaj za pomocą metody equals
+        System.out.println("Porównanie za pomocą metody equals:");
+        System.out.println(str1.equals(str2));
+        System.out.println();
     }
 }
